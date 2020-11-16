4bb2161f4adc88109b743bb5b57ad2b365ee7ee0
Mariel Latella Barros <mariel.latella@gmail.com>
2020-11-16 20:08:55 +0100

    Add find product by id using query parameters

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -31,5 +32,23 @@ class ProductController extends AbstractController
         $entityManager->persist($product);
         $entityManager->flush();
         return new Response('Saved new product ' . $product);
+    }
+
+    /**
+     * @Route("/find", name="productById")
+     * @param Request $request
+     * @return Response
+     */
+    public function findById(Request $request)
+    {
+        $id = $request->query->get("id");
+        $product = $this->getDoctrine()
+            ->getRepository(Product::class)
+            ->find($id);
+        if($product!=null) {
+            return new Response('Product with id: '.$id. ' was found:'. $product);
+        } else {
+            return new Response("Sorry, product with id ".$id ." was  not found :(", Response::HTTP_NOT_FOUND);
+        }
     }
 }
